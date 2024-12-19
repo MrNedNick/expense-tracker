00277abf29ee5c8fc2eeedaed066fa5b2b210077
MrNedNick <mrnediala@gmail.com>
2024-12-19 22:02:59 +0100

    edit store in option api

--- src/stores/expenses.js
+++ src/stores/expenses.js
@@ -1,21 +1,29 @@
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 
-export const useExpensesStore = defineStore('expenses', {
-  state: () => ({
-    expenses: [],
-  }),
+export const useExpensesStore = defineStore('expenses', () => {
 
-  actions: {
-    addExpense(expense) {
-      console.log('addExpense');
-      this.expenses.push({ ...expense, id: Date.now() });
-      console.log('expenses', this.expenses);
-    },
-    removeExpense(id) {
-      this.expenses = this.expenses.filter((expense) => expense.id !== id);
-    },
-    totalExpenses() {
-      return this.expenses.reduce((sum, expense) => sum + expense.amount, 0);
-    },
-  },
+  const expenses = ref([]);
+
+  const addExpense = (expense) => {
+    expenses.value.push({ ...expense, id: Date.now() });
+    console.log('Expense added:', expense);
+    console.log('Current expenses:', expenses.value);
+  };
+
+  const removeExpense = (id) => {
+    expenses.value = expenses.value.filter((expense) => expense.id !== id);
+    console.log(`Expense with id ${id} removed.`);
+  };
+
+  const totalExpenses = computed(() =>
+    expenses.value.reduce((sum, expense) => sum + expense.amount, 0)
+  );
+
+  return {
+    expenses,
+    addExpense,
+    removeExpense,
+    totalExpenses,
+  };
 });
